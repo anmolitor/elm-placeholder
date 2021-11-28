e524e4b96225784e619b6e6a0ebc7c25a277bb92
aewering <andreas.ewering@andrena.de>
2021-11-28 12:39:08 +0100

    add templates based on alphabetic key ordering and more

--- src/Placeholder/DoubleCurly.elm
+++ src/Placeholder/DoubleCurly.elm
@@ -1,4 +1,7 @@
-module Placeholder.DoubleCurly exposing (parsePlaceholder1, parsePlaceholder2, parsePlaceholder3, parsePlaceholder4)
+module Placeholder.DoubleCurly exposing
+    ( parsePlaceholder1, parsePlaceholder2, parsePlaceholder3, parsePlaceholder4
+    , parsePlaceholderAlph1, parsePlaceholderAlph2, parsePlaceholderAlph3, parsePlaceholderAlph4
+    )
 
 {-|
 
@@ -9,6 +12,7 @@ Parse Strings into functions using the {{double curly brackets}} placeholder syn
 For more detail, view the documentation in `Placeholder.Internal`.
 
 @docs parsePlaceholder1, parsePlaceholder2, parsePlaceholder3, parsePlaceholder4
+@docs parsePlaceholderAlph1, parsePlaceholderAlph2, parsePlaceholderAlph3, parsePlaceholderAlph4
 
 -}
 
@@ -50,3 +54,42 @@ parsePlaceholder3 =
 parsePlaceholder4 : String -> Result String (String -> String -> String -> String -> String)
 parsePlaceholder4 =
     Internal.parsePlaceholder4 syntax
+
+
+{-| Parse a `String` into a function substituting the argument at all positions marked by a key inside of double curly braces.
+
+    parsePlaceholderAlph1 "This is {{name}}s example (made by {{name}})" |> Result.map ((|>) "Andy")
+    --> Ok "This is Andys example (made by Andy)"
+
+-}
+parsePlaceholderAlph1 : String -> Result String (String -> String)
+parsePlaceholderAlph1 =
+    Internal.parsePlaceholderAlph1 syntax
+
+
+{-| Parse a `String` into a function substituting the argument at all positions marked by two keys inside of double curly braces.
+The arguments are substituted in alphabetical order of the keys.
+
+    parsePlaceholderAlph2 "{{b}} wrote a letter to {{a}}, then {{a}} wrote one back to {{b}}" |> Result.map (\f -> f "Joe" "Mary")
+    --> Ok "Mary wrote a letter to Joe, then Joe wrote one back to Mary"
+
+-}
+parsePlaceholderAlph2 : String -> Result String (String -> String -> String)
+parsePlaceholderAlph2 =
+    Internal.parsePlaceholderAlph2 syntax
+
+
+{-| Parse a `String` into a function substituting the argument at all positions marked by three keys inside of double curly braces.
+The arguments are substituted in alphabetical order of the keys.
+-}
+parsePlaceholderAlph3 : String -> Result String (String -> String -> String -> String)
+parsePlaceholderAlph3 =
+    Internal.parsePlaceholderAlph3 syntax
+
+
+{-| Parse a `String` into a function substituting the argument at all positions marked by four keys inside of double curly braces.
+The arguments are substituted in alphabetical order of the keys.
+-}
+parsePlaceholderAlph4 : String -> Result String (String -> String -> String -> String -> String)
+parsePlaceholderAlph4 =
+    Internal.parsePlaceholderAlph4 syntax
--- src/Placeholder/Internal.elm
+++ src/Placeholder/Internal.elm
@@ -1,6 +1,7 @@
 module Placeholder.Internal exposing
-    ( Template, parseTemplate, getPlaceholderNames, templateToString
+    ( Template, parseTemplate, getPlaceholderNames, getAlphabeticalPlaceholderNames, templateToString, mapPlaceholders
     , Syntax, parsePlaceholder1, parsePlaceholder2, parsePlaceholder3, parsePlaceholder4
+    , parsePlaceholderAlph1, parsePlaceholderAlph2, parsePlaceholderAlph3, parsePlaceholderAlph4
     )
 
 {-| This module allows defining placeholder parsers based on a non-supported `Syntax` as well as
@@ -9,12 +10,13 @@ using the internal `Template` type for different purposes.
 
 # Template
 
-@docs Template, parseTemplate, getPlaceholderNames, templateToString
+@docs Template, parseTemplate, getPlaceholderNames, getAlphabeticalPlaceholderNames, templateToString, mapPlaceholders
 
 
 # Syntax
 
 @docs Syntax, parsePlaceholder1, parsePlaceholder2, parsePlaceholder3, parsePlaceholder4
+@docs parsePlaceholderAlph1, parsePlaceholderAlph2, parsePlaceholderAlph3, parsePlaceholderAlph4
 
 -}
 
@@ -44,20 +46,39 @@ type Template
 
 
 type alias TemplateInternal =
-    { placeholders : List String
+    { placeholders : List ( List Int, String )
     , segments : NonEmpty String
     , syntax : Syntax
+    , holes : Int
     }
 
 
 emptyTemplate : Syntax -> String -> TemplateInternal
 emptyTemplate syntax begin =
-    { placeholders = [], segments = List.NonEmpty.singleton begin, syntax = syntax }
+    { placeholders = [], segments = List.NonEmpty.singleton begin, syntax = syntax, holes = 0 }
 
 
 addPlaceholder : String -> TemplateInternal -> TemplateInternal
 addPlaceholder placeholder template =
-    { template | placeholders = placeholder :: template.placeholders }
+    let
+        helper : List ( List Int, String ) -> List ( List Int, String )
+        helper list =
+            case list of
+                [] ->
+                    [ ( [ template.holes ], placeholder ) ]
+
+                (( pos, ph ) as head) :: rest ->
+                    case compare placeholder ph of
+                        LT ->
+                            ( [ template.holes ], placeholder ) :: list
+
+                        EQ ->
+                            ( template.holes :: pos, ph ) :: rest
+
+                        GT ->
+                            head :: helper rest
+    in
+    { template | placeholders = helper template.placeholders, holes = template.holes + 1 }
 
 
 addTextSegment : String -> TemplateInternal -> TemplateInternal
@@ -103,15 +124,34 @@ parseTemplate syntax =
 
 
 {-| Get the names of the placeholders in a `Template`.
+The names are ordered just like they are in the given template string.
 
     parseTemplate { startSymbol = "${", endSymbol = "}" } "${first} ${snd}"
         |> Result.map getPlaceholderNames
     --> Ok ["first", "snd"]
 
+    parseTemplate { startSymbol = "${", endSymbol = "}" } "${blub} ${bla} ${blub}"
+        |> Result.map getPlaceholderNames
+    --> Ok ["blub", "bla", "blub"]
+
 -}
 getPlaceholderNames : Template -> List String
 getPlaceholderNames (Template template) =
-    template.placeholders
+    List.concatMap (\( indices, str ) -> List.map (Tuple.pair str) indices) template.placeholders
+        |> List.sortWith (\( _, a1 ) ( _, a2 ) -> compare a2 a1)
+        |> List.map Tuple.first
+
+
+{-| Get the names of the placeholders in a `Template`, but sorted in alphabetical order and without duplicates.
+
+    parseTemplate { startSymbol = "${", endSymbol = "}" } "${blub} ${bla} ${blub}"
+        |> Result.map getAlphabeticalPlaceholderNames
+    --> Ok ["bla", "blub"]
+
+-}
+getAlphabeticalPlaceholderNames : Template -> List String
+getAlphabeticalPlaceholderNames (Template template) =
+    template.placeholders |> List.map Tuple.second
 
 
 {-| Convert a parsed `Template` back to its `String` representation.
@@ -125,9 +165,9 @@ For any template built with `Syntax` s,
 
 -}
 templateToString : Template -> String
-templateToString (Template { placeholders, segments, syntax }) =
+templateToString ((Template { placeholders, segments, syntax }) as template) =
     List.NonEmpty.head segments
-        :: List.map2 (\p s -> p ++ syntax.endSymbol ++ s) placeholders (List.NonEmpty.tail segments)
+        :: List.map2 (\p s -> p ++ syntax.endSymbol ++ s) (getPlaceholderNames template) (List.NonEmpty.tail segments)
         |> String.join syntax.startSymbol
 
 
@@ -140,18 +180,23 @@ templateToString (Template { placeholders, segments, syntax }) =
     --> Ok "This is Andys example"
 
     parsePlaceholder1 { startSymbol = "${", endSymbol = "}" } "No placeholder"
-    --> Err "Expected more placeholders."
+    --> Err "Expected 1 placeholders, but parsed a total of 0."
 
     parsePlaceholder1 { startSymbol = "${", endSymbol = "}" } "${multiple}${placeholders}"
-    --> Err "Expected less placeholders."
+    --> Err "Expected 1 placeholders, but parsed a total of 2."
 
 -}
 parsePlaceholder1 : Syntax -> String -> Result String (F1 String)
 parsePlaceholder1 syntax =
-    parseTemplate syntax >> Result.andThen (start f1 >> enough)
+    parseTemplate syntax
+        >> Result.andThen (expectHoles 1)
+        >> Result.map (\template -> l1 >> fillUnsafe template)
 
 
 {-| Parse a `String` into a function substituting the arguments at the two positions marked by the `Syntax`.
+Note that this function and the other `parsePlaceholderN` functions _always_ substitute elements in the order
+they appear in the template string. This also means that for duplicate placeholder keys, you need to use
+the `parsePlaceholderAlphN` family of functions instead.
 
     parsePlaceholder2 { startSymbol = "{", endSymbol = "}" } "{greeting} {target}"
         |> Result.map (\f -> f "Hello" "World")
@@ -160,21 +205,157 @@ parsePlaceholder1 syntax =
 -}
 parsePlaceholder2 : Syntax -> String -> Result String (F2 String)
 parsePlaceholder2 syntax =
-    parseTemplate syntax >> Result.andThen (start f2 >> oneMore >> enough)
+    parseTemplate syntax
+        >> Result.andThen (expectHoles 2)
+        >> Result.map (\template v1 -> l2 v1 >> fillUnsafe template)
 
 
 {-| Parse a `String` into a function substituting the arguments at the three positions marked by the `Syntax`.
 -}
 parsePlaceholder3 : Syntax -> String -> Result String (F3 String)
 parsePlaceholder3 syntax =
-    parseTemplate syntax >> Result.andThen (start f3 >> twoMore >> enough)
+    parseTemplate syntax
+        >> Result.andThen (expectHoles 3)
+        >> Result.map (\template v1 v2 -> l3 v1 v2 >> fillUnsafe template)
 
 
 {-| Parse a `String` into a function substituting the arguments at the four positions marked by the `Syntax`.
 -}
 parsePlaceholder4 : Syntax -> String -> Result String (F4 String)
 parsePlaceholder4 syntax =
-    parseTemplate syntax >> Result.andThen (start f4 >> twoMore >> oneMore >> enough)
+    parseTemplate syntax
+        >> Result.andThen (expectHoles 4)
+        >> Result.map (\template v1 v2 v3 -> l4 v1 v2 v3 >> fillUnsafe template)
+
+
+{-| Parse a `String` into a function substituting the argument at all positions marked with the same, only key
+This differs from `parsePlaceholder1` as you can see in the following example:
+
+    syntax : Syntax
+    syntax = { startSymbol = "{", endSymbol = "}" }
+
+    parsePlaceholder1 syntax "{greeting} {greeting}"
+    --> Err "Expected 1 placeholders, but parsed a total of 2."
+
+    parsePlaceholderAlph1 syntax "{greeting} {greeting}" |> Result.map ((|>) "Hello")
+    --> Ok "Hello Hello"
+
+-}
+parsePlaceholderAlph1 : Syntax -> String -> Result String (F1 String)
+parsePlaceholderAlph1 syntax =
+    parseTemplate syntax
+        >> Result.andThen (expectPlaceholderKeys 1)
+        >> Result.map (\template -> l1 >> fillAlphabeticalUnsafe template)
+
+
+{-| Parse a `String` into a function substituting the arguments at all positions marked with the two placeholder keys.
+The arguments for the keys are expected in alphabetical order.
+This differs from `parsePlaceholder2` as you can see in the following example:
+
+    syntax : Syntax
+    syntax = { startSymbol = "{", endSymbol = "}" }
+
+    parsePlaceholder2 syntax "{greeting} {friend}" |> Result.map (\f -> f "Hello" "World")
+    --> Ok "Hello World"
+
+    parsePlaceholderAlph2 syntax "{greeting} {friend}" |> Result.map (\f -> f "Hello" "World")
+    --> Ok "World Hello"
+
+-}
+parsePlaceholderAlph2 : Syntax -> String -> Result String (F2 String)
+parsePlaceholderAlph2 syntax =
+    parseTemplate syntax
+        >> Result.andThen (expectPlaceholderKeys 2)
+        >> Result.map (\template v1 -> l2 v1 >> fillAlphabeticalUnsafe template)
+
+
+{-| Parse a `String` into a function substituting the arguments at all positions marked with the three placeholder keys.
+The arguments for the keys are expected in alphabetical order.
+-}
+parsePlaceholderAlph3 : Syntax -> String -> Result String (F3 String)
+parsePlaceholderAlph3 syntax =
+    parseTemplate syntax
+        >> Result.andThen (expectPlaceholderKeys 3)
+        >> Result.map (\template v1 v2 -> l3 v1 v2 >> fillAlphabeticalUnsafe template)
+
+
+{-| Parse a `String` into a function substituting the arguments at all positions marked with the four placeholder keys.
+The arguments for the keys are expected in alphabetical order.
+-}
+parsePlaceholderAlph4 : Syntax -> String -> Result String (F4 String)
+parsePlaceholderAlph4 syntax =
+    parseTemplate syntax
+        >> Result.andThen (expectPlaceholderKeys 4)
+        >> Result.map (\template v1 v2 v3 -> l4 v1 v2 v3 >> fillAlphabeticalUnsafe template)
+
+
+{-| Change placeholder keys based on index and current key.
+
+    parseTemplate { startSymbol = "{", endSymbol = "}" } "{UPPER} case"
+        |> Result.map (mapPlaceholders (\_ -> String.toLower) >> templateToString)
+    --> Ok "{upper} case"
+
+-}
+mapPlaceholders : (Int -> String -> String) -> Template -> Template
+mapPlaceholders f (Template template) =
+    Template
+        { template
+            | placeholders =
+                List.indexedMap
+                    (\i ( indices, key ) ->
+                        ( indices, f i key )
+                    )
+                    template.placeholders
+        }
+
+
+expectHoles : Int -> Template -> Result String Template
+expectHoles expectedHoles ((Template { holes }) as template) =
+    if expectedHoles == holes then
+        Ok template
+
+    else
+        Err <|
+            "Expected "
+                ++ String.fromInt expectedHoles
+                ++ " placeholders, but parsed a total of "
+                ++ String.fromInt holes
+                ++ "."
+
+
+expectPlaceholderKeys : Int -> Template -> Result String Template
+expectPlaceholderKeys expectedNumberOfKeys ((Template { placeholders }) as template) =
+    if expectedNumberOfKeys == List.length placeholders then
+        Ok template
+
+    else
+        Err <|
+            "Expected "
+                ++ String.fromInt expectedNumberOfKeys
+                ++ " placeholder keys, but parsed ["
+                ++ (List.map Tuple.second placeholders |> String.join ", ")
+                ++ "]"
+
+
+fillUnsafe : Template -> List String -> String
+fillUnsafe (Template { segments, placeholders }) values =
+    List.NonEmpty.head segments
+        :: List.map2 (++) values (List.NonEmpty.tail segments)
+        |> String.join ""
+
+
+fillAlphabeticalUnsafe : Template -> List String -> String
+fillAlphabeticalUnsafe (Template { segments, placeholders }) values =
+    let
+        valuesInCorrectOrder =
+            List.map2 (\( indices, _ ) value -> List.map (Tuple.pair value) indices) placeholders values
+                |> List.concat
+                |> List.sortWith (\( _, a1 ) ( _, a2 ) -> compare a2 a1)
+                |> List.map Tuple.first
+    in
+    List.NonEmpty.head segments
+        :: List.map2 (++) valuesInCorrectOrder (List.NonEmpty.tail segments)
+        |> String.join ""
 
 
 type alias F1 a =
@@ -213,54 +394,21 @@ type alias F9 a =
     a -> F8 a
 
 
-f1 : F3 String
-f1 s1 s2 p1 =
-    s1 ++ p1 ++ s2
+l1 : String -> List String
+l1 =
+    List.singleton
 
 
-f2 : F5 String
-f2 s1 s2 s3 p1 p2 =
-    s1 ++ p1 ++ f1 s2 s3 p2
+l2 : String -> String -> List String
+l2 p1 p2 =
+    [ p1, p2 ]
 
 
-f3 : F7 String
-f3 s1 s2 s3 s4 p1 p2 p3 =
-    s1 ++ p1 ++ f2 s2 s3 s4 p2 p3
+l3 : String -> String -> String -> List String
+l3 p1 p2 p3 =
+    [ p1, p2, p3 ]
 
 
-f4 : F9 String
-f4 s1 s2 s3 s4 s5 p1 p2 p3 p4 =
-    s1 ++ p1 ++ f3 s2 s3 s4 s5 p2 p3 p4
-
-
-start : (String -> String -> b) -> Template -> Result String ( List String, b )
-start f (Template { segments }) =
-    ( List.NonEmpty.toList segments, f ) |> Ok >> twoMore
-
-
-oneMore : Result String ( List String, String -> b ) -> Result String ( List String, b )
-oneMore =
-    Result.andThen <|
-        \( segments, f ) ->
-            case segments of
-                [] ->
-                    Err "Expected more placeholders."
-
-                first :: rest ->
-                    Ok ( rest, f first )
-
-
-twoMore : Result String ( List String, String -> String -> b ) -> Result String ( List String, b )
-twoMore =
-    oneMore >> oneMore
-
-
-enough : Result String ( List String, b ) -> Result String b
-enough =
-    Result.andThen <|
-        \( segments, b ) ->
-            if List.isEmpty segments then
-                Ok b
-
-            else
-                Err <| "Expected less placeholders."
+l4 : String -> String -> String -> String -> List String
+l4 p1 p2 p3 p4 =
+    [ p1, p2, p3, p4 ]
